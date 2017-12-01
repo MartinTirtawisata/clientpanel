6e8768948793114a318790d13aef2f5e04b4dfe9
MartinTirtawisata <martin.tirtawisata@hotmail.com>
2017-11-30 20:58:31 -0800

    made components

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAaW_POJEdKnUc8XTqtItL98zlW-iN5TQc",
+    authDomain: "clientpanel-de764.firebaseapp.com",
+    databaseURL: "https://clientpanel-de764.firebaseio.com",
+    projectId: "clientpanel-de764",
+    storageBucket: "clientpanel-de764.appspot.com",
+    messagingSenderId: "1020113071850"
+  }
 };
