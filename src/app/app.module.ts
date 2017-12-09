00057702a897ddb469d8401850cb0e54bff3e5d0
MartinTirtawisata <martin.tirtawisata@hotmail.com>
2017-12-08 19:37:46 -0800

    finished project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,16 +25,22 @@ import { PageNotFoundComponent } from './components/page-not-found/page-not-foun
 
 import { ClientService } from "./services/client.service";
 import { AuthService } from "./services/auth.service";
+import { SettingsService } from "./services/settings.service";
+
+import { AuthGuard } from './guards/auth.guard';
+import { RegisterGuard } from './guards/register.guard';
 
 // Create Routes
 
 const appRoutes: Routes = [
-  {path:'', component: DashboardComponent},
-  {path:'register', component: RegisterComponent},
+  {path:'', component: DashboardComponent, canActivate:[AuthGuard]},
+  {path:'register', component: RegisterComponent, canActivate:[RegisterGuard]},
   {path:'login', component: LoginComponent},
-  {path:'add-client', component: AddClientComponent},
-  {path:'client/:id', component: ClientDetailsComponent},
-  {path:'edit-client/:id', component: EditClientComponent}
+  {path:'add-client', component: AddClientComponent, canActivate:[AuthGuard]},
+  {path:'client/:id', component: ClientDetailsComponent, canActivate:[AuthGuard]},
+  {path:'edit-client/:id', component: EditClientComponent, canActivate:[AuthGuard]},
+  {path:'settings', component: SettingsComponent, canActivate:[AuthGuard]},
+  {path:'**', component:PageNotFoundComponent}
 ];
 
 
@@ -66,7 +72,10 @@ const appRoutes: Routes = [
     AngularFireDatabaseModule,
     ClientService,
     AuthService,
-    FlashMessagesService
+    FlashMessagesService,
+    AuthGuard,
+    SettingsService,
+    RegisterGuard
   ],
   bootstrap: [AppComponent]
 })
